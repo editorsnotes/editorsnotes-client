640424684c94ec6e4d933eaacc79265ce391aa43
Patrick Golden <ptgolden0@gmail.com>
2015-06-12 16:01:26 -0400

    Add version number to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,10 +4,11 @@ var React = require('react')
 
 module.exports = React.createClass({
   render: function () {
+    var version = require('../../package.json').version
     return (
       <div className="footer">
         <div className="footer-inner container">
-          DEPLOYMENT INFO
+        {version}
         </div>
       </div>
     )
