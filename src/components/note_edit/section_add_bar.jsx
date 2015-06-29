a9e128d4a60ffc4641f4436a4d5f98a77a191683
Patrick Golden <ptgolden0@gmail.com>
2015-06-29 14:13:06 -0400

    Make section add bar sticky

--- src/components/note_edit/section_add_bar.jsx
+++ src/components/note_edit/section_add_bar.jsx
@@ -4,12 +4,35 @@ var React = require('react')
 
 module.exports = React.createClass({
   displayName: 'NoteSectionAddBar',
+  getInitialState: function () {
+    return { sticky: false }
+  },
+  componentDidMount: function () {
+    window.addEventListener('scroll', this.handleScroll);
+    this.handleScroll();
+  },
+  componentDidUnmount: function () {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  handleScroll: function () {
+    var el = React.findDOMNode(this)
+      , offsetTop = el.getBoundingClientRect().top
+
+    if (offsetTop < 0) {
+      this.setState({ sticky: true });
+    } else {
+      this.setState({ sticky: false });
+    }
+
+  },
   render: function () {
     var handles = require('./section_handles.jsx')
 
     return (
       <div id="section-add-bar-container">
-        <div id="citation-edit-bar" style={{ overflow: "auto" }}>
+        <div id="citation-edit-bar"
+            className={this.state.sticky ? 'sticky' : ''}
+            style={{ overflow: "auto" }} >
           <h4>
             Add section
             {' '}
