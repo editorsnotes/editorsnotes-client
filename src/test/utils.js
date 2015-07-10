5d9c18291bac8f0065bb118645c2eeb268cc4aef
Patrick Golden <ptgolden0@gmail.com>
2015-07-10 16:49:36 -0400

    Use tape for testing; bump {browser,watch}ify; remove testling
    Testling seems dead and leaves zombie processes which hinder its
    ability to function properly. `tape-run` seems to do basically the same
    thing, and it runs with less bugs.

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,55 +1,59 @@
 "use strict";
 
-var assert = require('assert')
-  , _ = require('underscore')
+var test = require('tape')
 
+test('Citation generator', function (t) {
+  t.plan(2);
 
-describe('Citation generator', function () {
-  var CitationGenerator = require('../utils/citation_generator');
+  var CitationGenerator = require('../utils/citation_generator')
+    , generator = new CitationGenerator()
+    , testData
 
-  it('should be able to be created', function () {
-    var testGenerator = new CitationGenerator();
-    assert.notEqual(testGenerator.engine, undefined);
-  });
+  testData = {
+    id: 'testing',
+    type: 'book',
+    title: 'Living My Life',
+    author: [{ family: 'Goldman', given: 'Emma' }],
+    issued: { raw: '1931' }
+  }
 
-  it('should be able to produce citations', function () {
-    var testGenerator = new CitationGenerator()
-      , testData = {
-        id: 'testing',
-        type: 'book',
-        title: 'Living My Life',
-        author: [{ family: 'Goldman', given: 'Emma' }],
-        issued: { raw: '1931' }
-      }
+  t.notEqual(
+    generator.engine,
+    undefined,
+    'should be able to be created'
+  );
 
-    assert.equal(
-      testGenerator.makeCitation(testData),
-      'Emma Goldman, <em>Living My Life</em>, 1931.'
-    )
-  });
+  t.equal(
+    generator.makeCitation(testData),
+    'Emma Goldman, <em>Living My Life</em>, 1931.',
+    'should be able to produce citations'
+  );
 });
 
-describe('Zotero => CSL converter', function () {
+test('Zotero => CSL converter', function (t) {
+  t.plan(1);
+
   var converter = require('../utils/zotero_to_csl')
+    , testData
+    , expected
 
-  it('should give me a CSL object when passed a Zotero object', function () {
-    var testData
-      , expected
+  testData = {
+    itemType: 'book',
+    title: 'Living My Life',
+    creators: [{ creatorType: 'author', firstName: 'Emma', lastName: 'Goldman' }],
+    date: '1931'
+  }
 
-    testData = {
-      itemType: 'book',
-      title: 'Living My Life',
-      creators: [{ creatorType: 'author', firstName: 'Emma', lastName: 'Goldman' }],
-      date: '1931'
-    }
+  expected = {
+    type: 'book',
+    title: 'Living My Life',
+    author: [{ family: 'Goldman', given: 'Emma' }],
+    issued: { raw: '1931' }
+  }
 
-    expected = {
-      type: 'book',
-      title: 'Living My Life',
-      author: [{ family: 'Goldman', given: 'Emma' }],
-      issued: { raw: '1931' }
-    }
-
-    assert.deepEqual(converter(testData), expected);
-  });
+  t.deepEqual(
+    converter(testData),
+    expected,
+    'should give me a CSL object when passed a Zotero object'
+  );
 });
