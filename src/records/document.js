fc7366995575be6ba516af41899ae6866983a332
Patrick Golden <ptgolden0@gmail.com>
2015-09-28 19:18:08 -0400

    Add missing `related_topics` field to document record

--- src/records/document.js
+++ src/records/document.js
@@ -7,5 +7,6 @@ var Immutable = require('immutable')
 module.exports = Immutable.Record({
   description: '',
   zotero_data: null,
+  related_topics: Immutable.List(),
   scans: Immutable.List()
 });
