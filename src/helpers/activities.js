3a31706d86c13eb5d9c9dbabed8ad294d75dcdfa
Patrick Golden <ptgolden0@gmail.com>
2015-09-28 20:02:53 -0400

    Add a function to group activities for the same items

---
+++ src/helpers/activities.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var Immutable = require('immutable')
+
+function activityURLAndAction(activity) {
+  return `${activity.get('action')}|${activity.get('url')}`
+}
+
+function groupActivities(activities) {
+  return activities
+    .groupBy(activityURLAndAction)
+    .toList()
+    .sortBy(activitiesForURL => activities.indexOf(activitiesForURL.first()))
+    .map(activitiesForURL => Immutable.Map({
+      count: activitiesForURL.size,
+      mostRecent: activitiesForURL.first(),
+      times: activitiesForURL.map(activity => activity.get('time'))
+    }))
+}
+
+module.exports = { groupActivities }
---
+++ src/test/helpers.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var test = require('tape')
+  , Immutable = require('immutable')
+
+test('Activities helpers', function (t) {
+  var activitiesHelpers = require('../helpers/activities')
+    , activitiesData = require('./fixtures/activity_list.json')
+
+  activitiesData = Immutable.fromJS(activitiesData)
+
+  t.plan(1)
+
+  t.deepEqual(
+    activitiesHelpers.groupActivities(activitiesData).toJS(),
+    [
+      {
+        count: 2,
+        mostRecent: {
+          "user": "patrick",
+          "project": "patrick_research",
+          "time": "2015-09-28T14:30:59.277143",
+          "type": "note",
+          "url": "/projects/patrick_research/notes/17/",
+          "title": "My first note",
+          "action": "changed"
+        },
+        times: ["2015-09-28T14:30:59.277143", "2015-09-28T14:30:03.566247"]
+      },
+      {
+        count: 1,
+        mostRecent: {
+          "user": "p",
+          "project": "patrick_research",
+          "time": "2015-09-28T11:29:53.710729",
+          "type": "topic",
+          "url": "/projects/patrick_research/topics/8/",
+          "title": "DSflkjasd",
+          "action": "added"
+        },
+        times: ["2015-09-28T11:29:53.710729"]
+      },
+      {
+        count: 1,
+        mostRecent: {
+          "user": "p",
+          "project": "patrick_research",
+          "time": "2015-09-28T14:29:48.158976",
+          "type": "note",
+          "url": "/projects/patrick_research/notes/17/",
+          "title": "My first note",
+          "action": "added"
+        },
+        times: ["2015-09-28T14:29:48.158976"]
+      }
+    ]
+  )
+});
