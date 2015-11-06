5e648ff3ec5a23be2887554f73387001e7d91065
Patrick Golden <ptgolden0@gmail.com>
2015-11-05 22:55:10 -0500

    Remove zotero_to_csl, which is in another package
    (also, bump that package)

--- src/test/utils.js
+++ src/test/utils.js
@@ -29,31 +29,3 @@ test('Citation generator', function (t) {
     'should be able to produce citations'
   );
 });
-
-test('Zotero => CSL converter', function (t) {
-  var converter = require('../utils/zotero_to_csl')
-    , testData
-    , expected
-
-  t.plan(1);
-
-  testData = {
-    itemType: 'book',
-    title: 'Living My Life',
-    creators: [{ creatorType: 'author', firstName: 'Emma', lastName: 'Goldman' }],
-    date: '1931'
-  }
-
-  expected = {
-    type: 'book',
-    title: 'Living My Life',
-    author: [{ family: 'Goldman', given: 'Emma' }],
-    issued: { raw: '1931' }
-  }
-
-  t.deepEqual(
-    converter(testData),
-    expected,
-    'should give me a CSL object when passed a Zotero object'
-  );
-});
