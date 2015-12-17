608bc87fe036fc31b3819b4ff659dcddc818d72d
Patrick Golden <ptgolden0@gmail.com>
2015-12-17 14:53:09 -0500

    Warn before leaving a page if there are unsaved changes

--- src/components/util/standalone_form.jsx
+++ src/components/util/standalone_form.jsx
@@ -1,10 +1,42 @@
 "use strict";
 
 var React = require('react')
+  , Immutable = require('immutable')
   , editableComponent = require('./editable_component.jsx')
 
-module.exports = function (Component, type) {
+
+const types = Immutable.Map([
+  [require('../../records/note'), 'note'],
+  [require('../../records/topic'), 'topic'],
+  [require('../../records/document'), 'document']
+])
+
+
+module.exports = function (Component, RecordType) {
   var StandaloneForm = React.createClass({
+    getInitialState() {
+      return { [types.get(RecordType)]: this.originalData() }
+    },
+
+    componentDidMount() {
+      window.onbeforeunload = () => {
+        var updated = this.state[types.get(RecordType)]
+          , original = this.originalData()
+
+        if (!updated.equals(original)) {
+          return 'There are unsaved pages on this page. Closing it will lose your changes.';
+        }
+      }
+    },
+
+    componentWillUnmount() {
+      window.onbeforeunload = null;
+    },
+
+    originalData() {
+      return new RecordType(this.props.data)
+    },
+
     isNew() {
       return !this.props.data
     },
@@ -15,30 +47,38 @@ module.exports = function (Component, type) {
         this.props.data.get('project')
     },
 
+    handleRecordChange(updatedRecord) {
+      this.setState({ [types.get(RecordType)]: updatedRecord });
+    },
+
     handleSaveSuccess(response) {
       var redirect = this.isNew() ?
         response.headers.get('Location') :
         this.props.data.get('url')
 
+      window.onbeforeunload = null;
       window.location.href = redirect;
     },
 
-    saveAndRedirect(data) {
+    saveAndRedirect() {
       var { save } = this.props
+        , updatedData = this.state[types.get(RecordType)]
         , id = this.isNew() ? null : this.props.data.get('id')
 
-      save(id, this.getProjectURL(), data).then(this.handleSaveSuccess);
+      save(id, this.getProjectURL(), updatedData).then(this.handleSaveSuccess);
     },
 
     render() {
       return (
         <Component
             {...this.props}
+            {...this.state}
             projectURL={this.getProjectURL()}
+            handleRecordChange={this.handleRecordChange}
             saveAndRedirect={this.saveAndRedirect} />
       )
     }
   });
 
-  return editableComponent(StandaloneForm, type)
+  return editableComponent(StandaloneForm, types.get(RecordType))
 }
