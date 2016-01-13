a8b4fc71823ceba64f25a789c1c8b3bf1b444d52
Patrick Golden <ptgolden0@gmail.com>
2016-01-13 17:15:09 -0500

    Make top bar take up whole editing page

--- src/components/shared/en_editor/component.jsx
+++ src/components/shared/en_editor/component.jsx
@@ -4,6 +4,23 @@ var React = require('react')
   , ReactDOM = require('react-dom')
 
 
+const TOOLBAR_HEIGHT = '4em';
+
+
+function TopBar({}) {
+  var style = { height: TOOLBAR_HEIGHT, lineHeight: TOOLBAR_HEIGHT, padding: '0 1em' }
+
+  return (
+    <div className="bg-gray border-box right-align" style={style}>
+      <div className="container">
+        <button className="btn bg-white btn-outline mr2">Help</button>
+        <button className="btn btn-primary">Save</button>
+      </div>
+    </div>
+  )
+}
+
+
 module.exports = React.createClass({
   displayName: 'ENContentEditor',
 
@@ -75,36 +92,42 @@ module.exports = React.createClass({
       , flex = leftWidth === rightWidth
 
     return (
-      <div className="flex-grow bg-white flex flex-stretch">
-        <div
-            ref="left"
-            className="relative"
-            style={{
-              width: flex ? 'auto' : leftWidth,
-              flex: flex ? '2 2 0' : 'none'
-            }}>
-          <TextEditor width={leftWidth} />
+      <div className="flex-grow flex flex-column">
+        <div className="flex-none">
+          <TopBar />
         </div>
 
-        <div
-            className="relative"
-            draggable="true"
-            onDragStart={this.handleDragStart}
-            onDragEnd={this.handleDragEnd}
-            onDrag={this.handleDrag}
-            style={{ width: '16px', cursor: 'col-resize', marginLeft: -6 }}>
-          <div className="absolute" style={{ width: '4px', background: '#ccc', height: '100%', left: '6px' }} />
-        </div>
+        <div className="flex-grow bg-white flex flex-stretch">
+          <div
+              ref="left"
+              className="relative"
+              style={{
+                width: flex ? 'auto' : leftWidth,
+                flex: flex ? '2 2 0' : 'none'
+              }}>
+            <TextEditor width={leftWidth} />
+          </div>
+
+          <div
+              className="relative"
+              draggable="true"
+              onDragStart={this.handleDragStart}
+              onDragEnd={this.handleDragEnd}
+              onDrag={this.handleDrag}
+              style={{ width: '16px', cursor: 'col-resize', marginLeft: -6 }}>
+            <div className="absolute" style={{ width: '4px', background: '#ccc', height: '100%', left: '6px' }} />
+          </div>
 
 
-        <div
-            ref="right"
-            className="relative"
-            style={{
-              width: flex ? 'auto' : rightWidth,
-              flex: flex ? '1 1 0' : 'none'
-            }}>
-          <Panes />
+          <div
+              ref="right"
+              className="relative"
+              style={{
+                width: flex ? 'auto' : rightWidth,
+                flex: flex ? '1 1 0' : 'none'
+              }}>
+            <Panes />
+          </div>
         </div>
       </div>
     )
--- src/components/shared/en_editor/text_editor.jsx
+++ src/components/shared/en_editor/text_editor.jsx
@@ -3,31 +3,14 @@
 var React = require('react')
 
 
-const TOOLBAR_HEIGHT = '4em';
-
-function TopBar({}) {
-  var style = { height: TOOLBAR_HEIGHT, lineHeight: TOOLBAR_HEIGHT, padding: '0 1em' }
-
-  return (
-    <div className="bg-gray border-box col-12 right-align" style={style}>
-      <button className="btn bg-white btn-outline mr2">Help</button>
-      <button className="btn btn-primary">Save</button>
-    </div>
-  )
-}
-
-
 function CodeMirror() {
   return (
     <div className="absolute bg-lightgray" style={{
       overflowY: 'scroll',
       left: 0,
       right: 0,
-      height: '100%'
+      height: '100%',
     }}>
-
-      I am codemirror
-
     </div>
   )
 }
@@ -50,9 +33,6 @@ module.exports = React.createClass({
         right: 0,
         height: '100%'
       }}>
-        <div className="flex-none">
-          <TopBar {...this.props} {...this.state} />
-        </div>
         <div className="flex-grow relative">
           <CodeMirror />
         </div>
