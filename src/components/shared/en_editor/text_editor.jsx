d96a9353bfa9809d4e0f9c3ddde31535ad65d4a2
Patrick Golden <ptgolden0@gmail.com>
2016-01-13 16:05:55 -0500

    Try making a sticky toolbar on scroll

--- src/components/shared/en_editor/text_editor.jsx
+++ src/components/shared/en_editor/text_editor.jsx
@@ -1,15 +1,41 @@
 "use strict";
 
-var React = require('react')
+var _ = require('underscore')
+  , React = require('react')
+  , ReactDOM = require('react-dom')
+  , classnames = require('classnames')
 
 
-function TopBar() {
-  return <div />
+const TOOLBAR_HEIGHT = '4em';
+
+function TopBar({ width, fixed }) {
+  var className
+    , style = { height: TOOLBAR_HEIGHT, lineHeight: TOOLBAR_HEIGHT }
+
+  className = classnames(
+    'bg-yellow',
+    fixed ? 'fixed' : 'col-12'
+  );
+
+  if (fixed) {
+    style.width = width;
+    style.top = 0;
+  }
+
+  return (
+    <div className={className} style={style}>
+      I am the toolbar
+    </div>
+  )
 }
 
 
 function CodeMirror() {
-  return <div />
+  return (
+    <div>
+      I am codemirror
+    </div>
+  )
 }
 
 
@@ -19,11 +45,38 @@ module.exports = React.createClass({
   propTypes: {
   },
 
+  getInitialState() {
+    return { fixed: false }
+  },
+
+  componentWillMount() {
+    this.handleScroll = _.debounce(this.handleScroll, 5);
+  },
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  handleScroll(e) {
+    var el = ReactDOM.findDOMNode(this)
+      , { top } = el.getBoundingClientRect()
+
+    this.setState({ fixed: top < 0 });
+  },
+
   render() {
+    var { fixed } = this.state
+
     return (
-      <div>
-        <TopBar />
-        <CodeMirror />
+      <div style={{ paddingTop: fixed ? TOOLBAR_HEIGHT : 0 }}>
+        <TopBar {...this.props} {...this.state} />
+        <div>
+          <CodeMirror />
+        </div>
       </div>
     )
   }
