44ea1b147a39048a83ec427833286078188e81e5
Patrick Golden <ptgolden0@gmail.com>
2016-06-29 12:21:44 -0400

    Update utils tests
    1. Linked Data tests had fallen out of line with changes I made to the
    API to return arrays of s-p-o triples instead of an N3.Store object.
    
    2. Uses blue-tape for testing promises.
    
    3. Doesn't ask a (non-browser) node to run CSL tests, as CSL is only
    run in the browser.

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,74 +1,75 @@
 "use strict";
 
-var test = require('tape')
+var test = require('blue-tape')
 
 
-test('Citation generator', function (t) {
-  var CitationGenerator = require('../utils/citation_generator')
-    , generator = new CitationGenerator()
-    , testData
+if (process.browser) {
+  test('Citation generator', function (t) {
+    var CitationGenerator = require('../utils/citation_generator')
+      , generator = new CitationGenerator()
+      , testData
 
-  t.plan(2);
+    t.plan(2);
 
-  testData = {
-    id: 'testing',
-    type: 'book',
-    title: 'Living My Life',
-    author: [{ family: 'Goldman', given: 'Emma' }],
-    issued: { raw: '1931' }
-  }
+    testData = {
+      id: 'testing',
+      type: 'book',
+      title: 'Living My Life',
+      author: [{ family: 'Goldman', given: 'Emma' }],
+      issued: { raw: '1931' }
+    }
 
-  t.notEqual(
-    generator.engine,
-    undefined,
-    'should be able to be created'
-  );
+    t.notEqual(
+      generator.engine,
+      undefined,
+      'should be able to be created'
+    );
 
-  t.equal(
-    generator.makeCitation(testData),
-    'Emma Goldman, <em>Living My Life</em>, 1931.',
-    'should be able to produce citations'
-  );
-});
+    t.equal(
+      generator.makeCitation(testData),
+      'Emma Goldman, <em>Living My Life</em>, 1931.',
+      'should be able to produce citations'
+    );
+  });
+}
 
 
-test('Generic shouldComponentUpdate function', function (t) {
-  var shouldUpdate = require('../utils/should_update')
-    , Immutable = require('immutable')
+test('Generic shouldComponentUpdate function', t => {
+  const shouldUpdate = require('../utils/should_update')
+      , Immutable = require('immutable')
 
   t.plan(4);
 
   t.equals(true, shouldUpdate({}, {a: 1}));
-
   t.equals(false, shouldUpdate({a: 1}, {a: 1}));
-
   t.equals(false, shouldUpdate(
     {a: Immutable.fromJS({ key: 'value'})},
     {a: Immutable.fromJS({ key: 'value' })}
   ));
-
-  t.equals(false, shouldUpdate({a: 1, b: 2}, {b: 2}, ['a']))
+  t.equals(false, shouldUpdate(
+    {a: 1, b: 2},
+    {b: 2},
+    ['a']
+  ));
 });
 
 
-test('LD Parser', function (t) {
-  var parseLD = require('../utils/parse_ld')
-    , handleErr = err => { console.error(err) }
+test('LD Parser', t => {
+  const parseLD = require('../utils/parse_ld')
 
-  t.plan(2);
-
-  parseLD('<http://example.com/a> <http://example.com/b> <http://example.com/c> .')
-    .then(store => {
-      t.equal(1, store.find().length);
-    }, handleErr);
-
-  parseLD({
-    '@id': 'http://example.com/a',
-    'http://example.com/b': { '@id': 'http://example.com/c' }
-  }).then(store => {
-    t.equal(1, store.find().length);
-  }, handleErr);
-
-
-
+  return Promise.resolve()
+    .then(() =>
+      parseLD('<http://example.com/a> <http://example.com/b> <http://example.com/c> .')
+        .then(store => {
+          t.equal(1, store.length, 'should parse turtle/ntriples into arrays of triple objects');
+        })
+    )
+    .then(() =>
+      parseLD({
+        '@id': 'http://example.com/a',
+        'http://example.com/b': { '@id': 'http://example.com/c' }
+      }).then(store => {
+        t.equal(1, store.length, 'should parse JSON-LD into arrays of triple objects');
+      })
+    )
 });
