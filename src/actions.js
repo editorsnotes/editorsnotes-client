225ab05d9cd9ba497ca72057b73e9d13eef156fa
Patrick Golden <ptgolden0@gmail.com>
2016-06-29 20:35:30 -0400

    Update navigation action to reflect new request/readyState pattern

--- src/actions.js
+++ src/actions.js
@@ -7,7 +7,8 @@ const Immutable = require('immutable')
 
 
 const {
-  REQUEST_API_RESOURCE
+  REQUEST_API_RESOURCE,
+  REQUEST_NAVIGATION,
 } = require('./types').actions
 
 const {
@@ -17,23 +18,33 @@ const {
 } = require('./types').readyStates
 
 
+function dispatchReadyState(dispatch, type, readyState, opts) {
+  return dispatch(Object.assign({}, opts, { type, readyState }));
+}
+
+
 function navigateToPath(router, path, req=null) {
   return dispatch => {
     const match = router.match(path)
-
-    dispatch(navigationRequest(path));
-
-    const { resource, makeTripleStore } = match.handler
+        , { resource, makeTripleStore } = match.handler
         , resourceURL = resource ? resource(path) : null
         , headers = {}
+
+    const updateReadyState = dispatchReadyState.bind(
+      null,
+      dispatch,
+      REQUEST_NAVIGATION
+    );
 
     if (req) {
       headers.Host = req.headers.host;
       headers.cookie = req.headers.cookie;
     }
 
+    updateReadyState(PENDING, { path });
+
     const promises = [
-      dispatch(fetchUser(headers)),
+      dispatch(fetchAPIResource('/me/')),
       resourceURL && dispatch(
         fetchAPIResource(resourceURL, headers, makeTripleStore)
       )
@@ -41,10 +52,10 @@ function navigateToPath(router, path, req=null) {
 
     return Promise.all(promises)
       .then(() => {
-        dispatch(navigationSucess(path, resourceURL));
+        updateReadyState(SUCCESS, { path });
       })
       .catch(err => {
-        dispatch(navigationError(path, err));
+        updateReadyState(FAILURE, { path, err });
       })
 
   }
@@ -54,15 +65,17 @@ function fetchAPIResource(url, opts={}, parseTriples=false) {
   return (dispatch) => {
     let statusCode
 
+    const updateReadyState = dispatchReadyState.bind(
+      null,
+      dispatch,
+      REQUEST_API_RESOURCE
+    );
+
     if (!process.browser) {
       url = global.API_URL + url;
     }
 
-    dispatch({
-      type: REQUEST_API_RESOURCE,
-      url,
-      readyState: PENDING,
-    })
+    updateReadyState(PENDING, { url });
 
     opts.headers = Object.assign({}, opts.headers, {
       Accept: 'application/ld+json'
@@ -80,28 +93,20 @@ function fetchAPIResource(url, opts={}, parseTriples=false) {
         Promise.resolve(parseTriples && parseLD(data))
       ]))
       .then(([data, triples]) => {
-        dispatch({
-          type: REQUEST_API_RESOURCE,
+        updateReadyState(SUCCESS, {
           url,
-          readyState: SUCCESS,
           statusCode,
-
           responseData: Immutable.fromJS(data),
           responseTriples: triples
         })
-
-        return [data, triples];
       })
       .catch(err => {
         // TODO: Log stacktrace
-        dispatch({
-          type: REQUEST_API_RESOURCE,
+        updateReadyState(FAILURE, {
           url,
-          readyState: FAILURE,
           statusCode: err.statusCode,
-
           responseError: err.data
-        })
+        });
       });
   }
 }
