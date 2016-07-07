4cbf8b94233ac01073610c037620e77c74fa26f5
Patrick Golden <ptgolden0@gmail.com>
2016-07-06 22:47:37 -0400

    Make new N3 parser on every request to parse LD

--- src/utils/parse_ld.js
+++ src/utils/parse_ld.js
@@ -1,11 +1,12 @@
 "use strict"
 
 const jsonld = require('jsonld')
-    , parser = require('n3').Parser()
 
 // if `text` is an object, it will be assumed to be JSON-LD. If not, it will
 // be parsed as is by N3, which allows turtle, TriG, n-triples, and n-quads.
 module.exports = function(text) {
+  const parser = require('n3').Parser()
+
   const rdfString = typeof text === 'object'
     ? jsonld.promises.toRDF(text, { format: 'application/nquads' })
     : text
